244133df03667091972b918c0a71a374fcfa1c36
Gopal <iamgopallohar@gmail.com>
2023-08-06 20:10:12 +0530

    create list

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const LOCAL_ACTIVE_LIST_ID_KEY = "active-todo-list-id";
 const listsWrapper = document.querySelector("[data-lists-wrapper]");
 const todoTemplate = document.querySelector("[data-todo-template]");
 const todosWrapper = document.querySelector("[data-todos-wrapper]");
+const listForm = document.querySelector("[data-list-form]");
+const listInput = document.querySelector("[data-list-input]");
+const todoForm = document.querySelector("[data-todo-form]");
+const todoInput = document.querySelector("[data-todo-input]");
 
 function save() {
   localStorage.setItem(LOCAL_LISTS_KEY, JSON.stringify(lists));
@@ -56,20 +60,39 @@ function renderLists() {
 }
 
 function renderTodos(list) {
+  todosWrapper.innerHTML = "";
   list.todos.forEach((todo) => {
     todosWrapper.append(createTodoElement(todo));
   });
+  if (list.todos.length === 0) {
+    todosWrapper.append("No Todos Here");
+  }
 }
 
 function render() {
   renderLists();
 }
 
+function submitListForm(e) {
+  e.preventDefault();
+  if (listInput.value === "" || listInput.value === null) return;
+  const list = createList(listInput.value);
+  lists.push(list);
+  activeListId = list.id;
+  listInput.value = "";
+  save();
+  render();
+  document
+    .querySelector(`[data-id="${list.id}"`)
+    .scrollIntoView({ behavior: "smooth" });
+}
+
 const lists = JSON.parse(localStorage.getItem(LOCAL_LISTS_KEY)) || [
   createList("My List"),
 ];
-const activeListId =
+let activeListId =
   localStorage.getItem(LOCAL_ACTIVE_LIST_ID_KEY) || lists[0].id;
 
 save();
 render();
+listForm.addEventListener("submit", submitListForm);
